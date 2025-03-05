53237e56dfc1c0932dccfa895a3613bde16dc255
fujib <fujib@192.168.10.143>
2025-03-05 23:10:04 +0900

    23:09　完了

---
+++ ch6_1/src/calcapp/logics/Calclogic.java
@@ -0,0 +1,15 @@
+package calcapp.logics;
+
+public class Calclogic {
+
+	public static int tasu(int a, int b) {
+		return (a + b);
+	}
+	public static int hiku(int a, int b) {
+		return (a - b);
+	
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
